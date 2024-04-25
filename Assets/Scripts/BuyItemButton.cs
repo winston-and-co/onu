b4d6f08787802b86457b0e84d31d9c5f1e7fdfc5
jaked332 <92128947+jaked332@users.noreply.github.com>
2024-04-24 16:34:34 -1000

    Changed the numbers of the prices
    changed the numbers of the prices rather than having all of them to be the same number.

--- Assets/Scripts/BuyItemButton.cs
+++ Assets/Scripts/BuyItemButton.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class BuyItemButton : MonoBehaviour
 {
     public CurrencyLabel currency;
     public int cost;
+    public TextMeshProUGUI costText;
 
 
     public void Start()
     {
         currency = FindObjectOfType<CurrencyLabel>();
+        costText.text = cost.ToString();
     }
 
     public void TryBuy()
