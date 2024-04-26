3201f239a645f20b73a97fc766fbed199e019503
Winston Co <winstonco@live.com>
2024-04-25 23:13:16 -1000

    disable end turn on enemy turn

--- Assets/Scripts/Core/Battle/Visual/TurnButton.cs
+++ Assets/Scripts/Core/Battle/Visual/TurnButton.cs
@@ -1,12 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TurnButton : MonoBehaviour
 {
-    [SerializeField] Entity player;
-    public void TryEndTurn()
+    [SerializeField] GameObject container;
+
+    void Awake()
     {
-        BattleEventBus.getInstance().tryEndTurnEvent.Invoke(player);
+        BattleEventBus.getInstance().startTurnEvent.AddListener(OnTurnStart);
+        GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    void OnTurnStart(Entity e)
+    {
+        if (e != PlayerData.GetInstance().Player) return;
+        GetComponent<Button>().interactable = true;
+    }
+
+    void OnClick()
+    {
+        if (GameMaster.GetInstance().current_turn_entity != PlayerData.GetInstance().Player) return;
+        GetComponent<Button>().interactable = false;
+        BattleEventBus.getInstance().tryEndTurnEvent.Invoke(PlayerData.GetInstance().Player);
     }
 }
